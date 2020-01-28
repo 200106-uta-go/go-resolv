0f030bb1b9cf0b8a35393945c2f8e42292fd8c51
MehrabRahman <mehrab.rahman@revature.com>
2020-01-28 18:17:22 +0000

    second commit

--- main.go
+++ main.go
@@ -2,20 +2,23 @@ package main
 
 import "net"
 
-import "fmt"
-
-import "log"
-
 func main() {
-	port, err := net.LookupPort("tcp", "ssh")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Println(port)
+	// port, err := net.LookupPort("tcp", "ssh")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// fmt.Println(port)
 
-	addrs, _ := net.LookupHost("www.facebook.com")
-	fmt.Println(addrs)
+	// addrs, _ := net.LookupHost("www.facebook.com")
+	// fmt.Println(addrs)
 
-	ints, _ := net.Interfaces()
-	fmt.Println(ints)
+	// ints, _ := net.Interfaces()
+	// fmt.Println(ints)
+
+	tcpAddr, _ := net.ResolveTCPAddr("tcp", "localhost:8080")
+	tcpConn, _ := net.DialTCP("tcp", nil, tcpAddr)
+	defer tcpConn.Close()
+	buffer := make([]byte, 1024)
+	buffer = []byte("/ GET HTTP1.1 \r\n\r\n")
+	tcpConn.Write(buffer)
 }
